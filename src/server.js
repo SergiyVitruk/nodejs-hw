200fdf911d7e226e835a5ff72bd8dae3e2ceb179
SergiyVitruk <vitruk.sergiy@gmail.com>
2025-09-17 00:49:48 +0300

    add base message

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import express from "express";
-import cors from "cors";
-import pino from "pino-http";
-import "dotenv/config";
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
@@ -10,42 +10,46 @@ app.use(express.json());
 app.use(cors());
 app.use(
   pino({
-    level: "info",
+    level: 'info',
     transport: {
-      target: "pino-pretty",
+      target: 'pino-pretty',
       options: {
         colorize: true,
-        translateTime: "HH:MM:ss",
-        ignore: "pid,hostname",
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
         messageFormat:
-          "{req.method} {req.url} {res.statusCode} - {responseTime}ms",
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
         hideObject: true,
       },
     },
-  })
+  }),
 );
 
-app.get("/notes", (req, res) => {
-  res.status(200).json({ message: "Retrieved all notes" });
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello node.js' });
 });
 
-app.get("/notes/:noteId", (req, res) => {
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
   const { noteId } = req.params;
   res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
-app.get("/test-error", (req, res) => {
-  throw new Error("Simulated server error");
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
 });
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Route not found" });
+  res.status(404).json({ message: 'Route not found' });
 });
 
 app.use((err, req, res, next) => {
-  console.error("Error:", err.message);
+  console.error('Error:', err.message);
   res.status(500).json({
-    message: "Simulated server error",
+    message: 'Simulated server error',
   });
 });
 
