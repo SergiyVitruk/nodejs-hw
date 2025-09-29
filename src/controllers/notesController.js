0c08832bf26641c18007560f955c32ae9cc6f4ab
SergiyVitruk <vitruk.sergiy@gmail.com>
2025-09-29 18:36:36 +0300

    notesController fix

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -7,7 +7,7 @@ export const getAllNotes = async (req, res) => {
 
   const skip = (page - 1) * perPage;
 
-  const notesQuery = Note.find({ userId: req.user._id });
+  const notesQuery = Note.find();
 
   if (search) {
     notesQuery.where({
